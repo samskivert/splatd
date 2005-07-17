9383bb4b96d6f61ce6de99354957b9d0ab89bc71
landonf <landonf@10ef7b54-e64e-0410-915f-319f60a19af1>
2005-07-17 02:55:53 +0000

    r243@timor:  landonf | 2005-07-16 19:55:03 -0700   r17@luxo:  landonf | 2005-07-16 19:52:25 -0700   Add license header, full tilt

--- Code/lids/trunk/lids/daemon.py
+++ Code/lids/trunk/lids/daemon.py
@@ -1,19 +1,40 @@
-# All Rights Reserved.
+# daemon.py
+# LIDS Daemon Support.
+# Author: Will Barton <wbb4@opendarwin.org>
+#
+# Copyright (c) 2005 Three Rings Design, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright owner nor the names of contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
 # 
-# Author: Will Barton
-#
-# lids - LDAP Information Distribution System
-# A library for retrieving, formatting, and updating information stored in 
-# an LDAP database
-#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 # This file contains everything necessary to run a Daemon that
 # distributes information from LDAP using helper classes based on the
 # frequency specification of sections in a lid.conf file.
 #
 # lids.daemon(conf) is all that should be necessary to call, where conf
 # is a lids.parse_config()-parsed configuration file dictionary.
-#
-
 
 import functions
 import classes
