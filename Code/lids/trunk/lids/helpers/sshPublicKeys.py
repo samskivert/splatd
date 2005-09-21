8821b25b3ffc3a346075359add518b9deabdddd9
landonf <landonf@10ef7b54-e64e-0410-915f-319f60a19af1>
2005-09-21 00:11:14 +0000

    Clean up error handling/reporting between ssh writing subprocess and parent

--- Code/lids/trunk/lids/helpers/sshPublicKeys.py
+++ Code/lids/trunk/lids/helpers/sshPublicKeys.py
@@ -1,7 +1,9 @@
 # sshPublicKeys.py vi:ts=4:sw=4:expandtab:
 #
 # LDAP SSH Public Key Helper.
-# Author: Will Barton <wbb4@opendarwin.org>
+# Authors:
+#       Will Barton <wbb4@opendarwin.org>
+#       Landon Fuller <landonf@threerings.net>
 #
 # Copyright (c) 2005 Three Rings Design, Inc.
 # All rights reserved.
@@ -30,14 +32,19 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-__author__ = "Will Barton"
-
 import sys, os, logging
 
 import lids
 from lids import plugin
 
 logger = logging.getLogger(lids.LOG_NAME)
+
+# Sub-process result codes
+SSH_ERR_NONE = 0
+SSH_ERR_MISC = 1
+SSH_ERR_PRIVSEP = 2
+SSH_ERR_MKDIR = 3
+SSH_ERR_WRITE = 4
 
 class Writer(plugin.Helper):
     def work(self, ldapEntry):
@@ -59,31 +66,72 @@ class Writer(plugin.Helper):
     
         logger.info("Writing %s key to %s" % (key_type, filename))
 
-        # Fork and seteuid to write the files
-        if not os.fork():
+        # Fork and setuid to write the files
+        pipe = os.pipe()
+        outf = os.fdopen(pipe[1], 'w')
+        inf = os.fdopen(pipe[0], 'r')
+
+        pid = os.fork()
+        if (pid == 0):
+            # Drop privs
             try:
                 os.setgid(int(attributes.get("gidNumber")[0]))
                 os.setuid(int(attributes.get("uidNumber")[0]))
             except OSError, e:
-                raise plugin.LIDSPluginError, "Failed to drop privileges, %s" % e
+                print str(e)
+                outf.write(str(e) + '\n')
+                outf.close()
+                os._exit(SSH_ERR_PRIVSEP)
 
             try:
                 # Make sure the directory exists
                 dir = os.path.split(filename)[0]
                 if not os.path.exists(dir): os.makedirs(dir)
             except OSError, e:
-                raise plugin.LIDSPluginError, "Failed to create SSH directory '%s', %s" % (dir, e)
+                outf.write(str(e) + '\n')
+                outf.close()
+                os._exit(SSH_ERR_MKDIR)
 
             try:
                 f = open(filename, "w+")
                 f.write(contents)
                 f.close()
             except IOError, e:
-                raise plugin.LIDSPluginError, "Failed to write SSH key, %s" % (e)
+                outf.write(str(e) + '\n')
+                outf.close()
+                os._exit(SSH_ERR_WRITE)
 
-            sys.exit()
+            os._exit(SSH_ERR_NONE)
+        else:
+            while (1):
+                try:
+                    result = os.waitpid(pid, 0)
+                except OSError, e:
+                    import errno
+                    if (e.errno == errno.EINTR):
+                        continue
+                    raise
+                break
 
-        os.wait()
+            status = os.WEXITSTATUS(result[1])
+
+        errstr = inf.readline()
+        outf.close()
+        inf.close()
+
+        # Handle the error conditions
+        if (status == SSH_ERR_NONE):
+            return
+
+        if (status == SSH_ERR_PRIVSEP):
+            raise plugin.LIDSPluginError, "Failed to drop privileges, %s" % errstr
+
+        if (status == SSH_ERR_MKDIR):
+            raise plugin.LIDSPluginError, "Failed to create SSH directory '%s', %s" % errstr
+
+        if (status == SSH_ERR_WRITE):
+            raise plugin.LIDSPluginError, "Failed to write SSH key, %s" % errstr
+
 
     # A list of modifyable attributes for this helper, 
     # for interactive modifications (i.e. web interface)
